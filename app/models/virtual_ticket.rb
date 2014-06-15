4f4610d4b76435acb70768e4eddc737960ed10ea
Andrea Leopardi <an.leopardi@gmail.com>
2014-06-15 12:39:20 +0200

    Take advantage of new libs in VirtualTicket

--- app/models/virtual_ticket.rb
+++ app/models/virtual_ticket.rb
@@ -1,15 +1,19 @@
 require 'securerandom'
+require 'qr_code_fetcher'
 
 class VirtualTicket < ActiveRecord::Base
-  belongs_to :user
-
   VALID_FOR = 90.minutes
 
-  before_create :generate_code
+  belongs_to :user
+
+  before_create :generate_code, :fetch_qr_code
 
   validate :code, presence: true, uniqueness: true
 
   has_attached_file :qr_code
+  validates_attachment :qr_code, content_type: {
+    content_type: %w(image/png image/jpeg)
+  }
 
   # A ticket is expired if `VALID_FOR` time is passed since its creation.
   def expired?
@@ -19,13 +23,18 @@ class VirtualTicket < ActiveRecord::Base
   private
 
   # Generate a 40 characters base64 (but url-safe, which is [a-zA-Z0-9_\-]).
-  # Keep generating it in case a duplicate is generated.
-  # Checking for the record's validity is enough since the presence *and*
-  # uniqueness of the `code` attribute.
+  # Keep generating it in case a duplicate is generated; checking for the
+  # record's validity is enough since the presence *and* # uniqueness of the
+  # `code` attribute.
   def generate_code
     loop do
       self.code = SecureRandom.urlsafe_base64(40)
-      break if self.valid?
+      break if valid?
     end
+  end
+
+  # Fetch a QR code over the internet and store it in the `qr_code` attribute.
+  def fetch_qr_code
+    self.qr_code = QrCodeFetcher.qr_code(code)
   end
 end
--- test/models/virtual_ticket_test.rb
+++ test/models/virtual_ticket_test.rb
@@ -9,10 +9,14 @@ class VirtualTicketTest < ActiveSupport::TestCase
     assert t.expired?
   end
 
-  test 'code generation before creation' do
+  test 'creation callbacks' do
     ticket = VirtualTicket.create
     assert_not_nil ticket.code
+    assert_not_nil ticket.qr_code
+  end
 
+  test 'code generation before creation' do
+    ticket = VirtualTicket.create
     new_ticket = VirtualTicket.create code: ticket.code
     assert_not_equal ticket.code, new_ticket.code
   end
